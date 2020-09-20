a7d48328aa4cd625bc3c4ac6249bbcf140db310c
SeleznevaOS <71550739+SeleznevaOS@users.noreply.github.com>
2020-09-20 22:00:12 +0300

    Delete Num2.cpp

--- Num2.cpp
+++
@@ -1,9 +0,0 @@
-﻿#include <stdio.h>
-int main() {
-	int a, b, c, x;
-	printf("Еnter three numbers: \n");
-	scanf_s("%d%d%d", &a, &b, &c);
-	x = a + b + c;
-	printf("Result is %d+%d+%d=%d\n", a, b, c, x);
-	return 0;
-}
