e6b80f33b7d69da6ac1e4f7cfc2a1427c405d067
SeleznevaOS <71550739+SeleznevaOS@users.noreply.github.com>
2020-09-20 21:57:55 +0300

    Add files via upload

---
+++ Num3.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int main() {
+
+	int a,b,c,x;
+	printf("Enter three numbers: \n");
+	scanf_s("%d%d%d", &a,&b,&c);
+	x = a+b+c;
+	printf("Result is %d+%d+%d=%d\n", a,b,c,x);
+	
+
+	
+	return 0;
+
+}
