ba8e8b1b3fd75b2757cccc9650f2b3f41c3d4cc2
SeleznevaOS <71550739+SeleznevaOS@users.noreply.github.com>
2020-09-20 22:03:01 +0300

    Add files via upload

---
+++ Num2.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int main() {
+
+	int a, b, x;
+	printf("Еnter two numbers: \n");
+	scanf_s("%d%d", &a, &b);
+	x = a + b;
+	printf("Result is %d+%d=%d\n", a, b, x);
+
+
+
+	return 0;
+
+}
